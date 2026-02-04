5dba7bbcc1d10883aee1d5dc12fe22703d76fd9d
mzattyz12 <mzattyz12@gmail.com>
2026-02-04 15:22:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-package com.your.package.name // Replace with your package name
-
-import android.app.Application;
-import kotlinx.coroutines.CoroutineScope;
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
-import com.onesignal.OneSignal
-import com.onesignal.debug.LogLevel
-
-class ApplicationClass : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        // Enable verbose logging for debugging (remove in production)
-        OneSignal.Debug.logLevel = LogLevel.VERBOSE
-        // Initialize with your OneSignal App ID
-        OneSignal.initWithContext(this, "332721bb-21df-4486-8552-3f36a1899772")
-        // Use this method to prompt for push notifications.
-        // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
-        CoroutineScope(Dispatchers.IO).launch {
-            OneSignal.Notifications.requestPermission(true)
-        }
-    }
-}
+<!-- PushAlert Unified Code -->
+<script type="text/javascript">
+    (function(d, t) {
+        var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+        g.src = "https://cdn.pushalert.co/unified_a2ff9d3510c8693d1baed746524fc762.js";
+        s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+</script>
+<!-- End PushAlert Unified Code -->
